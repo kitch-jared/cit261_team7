f6e6208d3f9ea6bb471fe9976539917da1983697
Maxine <Maxine@Sini>
2015-10-08 12:07:10 -0700

    Merge origin/master
    Conflicts:
    	src/hurricane_game/Hurricane_game.java

--- src/model/MainMap.java
+++ src/model/MainMap.java
@@ -13,4 +13,56 @@ import java.io.Serializable;
  */
 public class MainMap implements Serializable{
     
+    private int numRows;
+    private int numColumns;
+    private Location[][] matrix;
+
+    public MainMap() {
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public void setNumRows(int numRows) {
+        this.numRows = numRows;
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    public void setNumColumns(int numColumns) {
+        this.numColumns = numColumns;
+    }
+
+    
+
+    public Location[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(Location[][] matrix) {
+        this.matrix = matrix;
+    }
+    ////////////////////////////////////////////
+    // METHOD
+    ///////////////////////////////////////////
+    
+    public void intialize() {
+        numRows = 3;
+        numColumns = 4;
+        
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numColumns; col++) {
+                Location newLocation = Location();
+                newLocation.setCol(col);
+                newLocation.setRow(row);
+            }
+        }
+         
+    }
+
+    
 }
+
