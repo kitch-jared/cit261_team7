ae78960444fac39cd01f891220a076c532363153
Maxine <Maxine@Sini>
2015-10-08 10:12:45 -0700

    Testing Commit

--- src/hurricane_game/Hurricane_game.java
+++ src/hurricane_game/Hurricane_game.java
@@ -17,7 +17,7 @@ public class Hurricane_game {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello");
+        System.out.println("Hello World!");
     }
     
 }
