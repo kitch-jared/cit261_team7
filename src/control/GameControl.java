10ea3097b89beceec09ad3ae6d487593853aebb7
Maxine <Maxine@Sini>
2015-11-06 10:01:36 -0800

    Added createNewGame function/method

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -5,6 +5,7 @@
  */
 package control;
 
+import java.util.Scanner;
 import model.Player;
 
 /**
@@ -13,8 +14,25 @@ import model.Player;
  */
 public class GameControl {
     
-    public void startNewGame() {
+    public void createNewGame(Player name) {
         
+        Scanner in = new Scanner(System.in);
+        
+        String input ="";
+        
+        char rtn = 0;
+        
+        rtn = input.toUpperCase().charAt(0);
+        
+        if((rtn !='y') && (rtn != 'Y') && (rtn != 'n') && (rtn !='N')){
+            System.out.println(name + ", you entered" + rtn);
+            System.out.println("Please Y for yes or N for no");
+        }
+        else {
+            System.out.println(name + "Welcome to your game. The clock starts now!");
+            
+            
+        }
     }
     
     public void initializeMap() {
