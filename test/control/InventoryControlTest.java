57780b9b27d75b2238817ecc66f4d7fcc2432932
Maxine <Maxine@Sini>
2015-11-13 01:03:18 -0800

    Not sure I've changed anything. I may not have committed changes from last week when I pulled at that time.

--- test/control/InventoryControlTest.java
+++ test/control/InventoryControlTest.java
@@ -127,6 +127,7 @@ public class InventoryControlTest {
         // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
     }
+}
 
 //    @Test
 //    public void testPurchaseItems() {
